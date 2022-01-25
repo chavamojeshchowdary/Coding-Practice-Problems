be18dc07f750dad0c8bcdf4f2f60830ba65870d7
chavamojeshchowdary <82503704+chavamojeshchowdary@users.noreply.github.com>
2022-01-25 11:24:02 +0530

    Create Container With Most Water.cpp

---
+++ Arrays/Container With Most Water.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i=0,j=height.size()-1,m=0,a;
+        while(i<j)
+        {
+            a=(j-i)*min(height[i],height[j]);
+            m=max(a,m);
+            if(height[i]<=height[j])
+                i++;
+            else
+                j--;
+        }
+        return m;
+    }
+};
